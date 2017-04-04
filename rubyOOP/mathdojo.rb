f249a5a9de9117c38b058f152ecd5dc105a82068
amarinas <ali.marinas@icloud.com>
2017-04-04 10:37:57 -0700

    completed mathdojo

---
+++ rubyOOP/mathdojo.rb
@@ -0,0 +1,26 @@
+# need an add
+# need subtract
+# need an result
+
+
+class MathDojo
+  attr_reader :result
+  def initialize
+    @result = 0
+  end
+  def add (*val)
+    @result += val.flatten.reduce(0, :+)
+    self
+  end
+
+  def subtract (*val)
+    @result -= val.flatten.reduce(0, :+)
+    self
+  end
+
+end
+
+challenge1 = MathDojo.new.add(2).add(2, 5).subtract(3, 2).result # => 4
+challenge2 = MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).result # => 23.15
+puts challenge1
+puts challenge2
