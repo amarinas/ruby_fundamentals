65b8484648fb88bb418db14ed4cff2329a559586
amarinas <ali.marinas@icloud.com>
2017-04-13 17:32:17 -0700

    comleted progect_rgr

---
+++ TDD/project_rgr/project_spec.rb
@@ -0,0 +1,11 @@
+Class Project
+  attr_accessor :name, :description,:owner
+
+  def initialize name, desc
+    @name =name
+    @description = desc
+  end
+  def elevator_pitch
+    "#{@name},#{@description}"
+  end
+end
